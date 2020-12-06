4231d23fa30a7ef5db05ecba64dfa8127e6abe19
Ben Wart <benwart@gmail.com>
2020-12-05 22:20:22 -0800

    using set.intersection instead

--- 2020/day06/exercise_2.py
+++ 2020/day06/exercise_2.py
@@ -36,19 +36,19 @@ In this example, the sum of these counts is 3 + 0 + 1 + 1 + 1 = 6.
 For each group, count the number of questions to which everyone answered "yes". What is the sum of those counts?
 """
 
+# Not needing as set.intersection(*sets) does all this (likely faster)
+#
+# def get_answers(anaswers):
+#     size = len(answers)
+#     counts = {chr(i): 0 for i in range(97, 123)}
+#     for a in answers:
+#         for c in a:
+#             counts[c] += 1
 
-def get_answers(anaswers):
-    size = len(answers)
-    counts = {chr(i): 0 for i in range(97, 123)}
-    for a in answers:
-        for c in a:
-            counts[c] += 1
-
-    # print([c for c, l in counts.items() if l > 0])
-    output = "".join([c for c, l in counts.items() if l == size])
-    # print(output)
-    return output
-
+#     # print([c for c, l in counts.items() if l > 0])
+#     output = "".join([c for c, l in counts.items() if l == size])
+#     # print(output)
+#     return output
 
 groups = []
 
@@ -56,14 +56,14 @@ with open("./data/full") as f:
     answers = []
     for line in f:
         if line == "\n":
-            groups.append(get_answers(answers))
+            groups.append(set.intersection(*answers))
             answers = []
             continue
 
-        answers.append([c for c in line.rstrip()])
+        answers.append(set([c for c in line.rstrip()]))
 
     if len(answers) > 0:
-        groups.append(get_answers(answers))
+        groups.append(set.intersection(*answers))
 
 # print(groups)
 print(f"Sum of counts: {sum(map(lambda x: len(x), groups))}")
