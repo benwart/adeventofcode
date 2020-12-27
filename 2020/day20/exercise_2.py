2d900a657bb795c023459777326f079742735cc1
Ben Wart <benwart@gmail.com>
2020-12-26 20:34:06 -0800

    2020 Day 20 found all rough water

--- 2020/day20/exercise_2.py
+++ 2020/day20/exercise_2.py
@@ -74,7 +74,7 @@ def transform_first_corner(tile, matches):
             debug.append(" -> right")
             tile.apply_transform([np.fliplr])
 
-        print("".join(debug))
+        # print("".join(debug))
 
 
 def find_side_tile(tile_matches, side):
@@ -132,7 +132,44 @@ def place_tiles(coords, current):
     place_side_tile(coords, current, "bottom")
 
 
-tiles = {t.tile_id: t for t in parse_input("./data/example1")}
+def count_waves_not_monsters(mosaic):
+    monster = np.array(
+        (
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+            [1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1],
+            [0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0],
+        ),
+        dtype=np.int,
+    )
+    monster_pos = {(y, x) for y, x in zip(*np.where(monster == 1))}
+    monster_shape = Coordinates(*monster.shape)
+    image_shape = Coordinates(*mosaic.shape)
+
+    monster_found = False
+    for i in range(8):
+        all_rough_water_pos = {(y, x) for y, x in zip(*np.where(mosaic == 1))}
+        for y in range(image_shape.y - monster_shape.y):
+            for x in range(image_shape.x - monster_shape.x):
+                cropped = mosaic[y : y + monster_shape.y, x : x + monster_shape.x]
+                rough_water_pos = set()
+                for cy, row in enumerate(cropped):
+                    for cx, char in enumerate(row):
+                        if char == 1:
+                            rough_water_pos.add((cy, cx))
+                if monster_pos.issubset(rough_water_pos):
+                    monster_found = True
+                    for y_, x_ in monster_pos & rough_water_pos:
+                        pos = (y + y_, x + x_)
+                        all_rough_water_pos.remove(pos)
+        if monster_found:
+            break
+        mosaic = np.rot90(mosaic)
+        if i == 4:
+            mosaic = np.flip(mosaic, 1)
+    return len(all_rough_water_pos)
+
+
+tiles = {t.tile_id: t for t in parse_input("./data/full")}
 
 side_length = int(sqrt(len(tiles)))
 matches = match_edges(list(tiles.values()))
@@ -180,7 +217,11 @@ for y, row in enumerate(mosaic_layout):
         x_end = x_start + cropped_shape.x
 
         window = mosaic[y_start:y_end, x_start:x_end]
-        window[:8, :8] = tiles[mosaic_layout[y, x]].cropped
+        window[: cropped_shape.y, : cropped_shape.x] = tiles[
+            mosaic_layout[y, x]
+        ].cropped
 
 
-print_array(mosaic)
+wave_count = count_waves_not_monsters(mosaic)
+
+print(wave_count)
