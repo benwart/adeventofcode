91698b8f65a3f91b3a3ae27b6f6b6266cd3009cb
Ben Wart <benwart@gmail.com>
2021-12-03 22:21:51 -0800

    merge

--- 2021/day03/exercise_1.py
+++ 2021/day03/exercise_1.py
@@ -16,19 +16,10 @@ for i, line in enumerate(parse_strs("./2021/day03/data/full")):
     for j in range(0, width):
         bits[j][int(line[j])] += 1
 
-<<<<<<< HEAD
 # print(bits)
 
 g = int("".join(["0" if bits[i][0] > bits[i][1] else "1" for i in range(0, width)]),2)
 e = int("".join(["0" if bits[i][0] <= bits[i][1] else "1" for i in range(0, width)]),2)
 
 print(f"g: {g}, e: {e}, g*e: {g*e}")
-=======
-print(bits)
-
-g = int("".join(["0" if bits[i][0] > bits[i][1] else "1" for i in range(0, width)]),2)
-e = int("".join(["0" if bits[i][0] < bits[i][1] else "1" for i in range(0, width)]),2)
-
-print(f"g={g}, e={e}, g*e={g*e}")
->>>>>>> e49e15f355874a72b686e3aa660f3105c2a42f7c
     
