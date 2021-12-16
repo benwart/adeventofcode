75754640f622c13182728a5c6f3ae3f968d24a8b
Ben Wart <benwart@gmail.com>
2021-12-15 21:40:53 -0800

    2021 day 14

--- 2021/day14/exercise_2.py
+++ 2021/day14/exercise_2.py
@@ -1,42 +1,67 @@
 #!/usr/bin/env python3
 
 from collections import defaultdict
-from llist import dllist
 from parser import parse_data
 
 
-def iteration(step, insertions, state):
-    for node in state.iternodes():
-        if node.prev is not None:
-            search = "".join([node.prev.value, node.value])
-            insert = insertions[search]
-            state.insertbefore(insert, node)
+def get_pairs(template):
+    pairs = defaultdict(lambda:0)
 
-    if step % 10 == 0:
-        print(".")
-    else:
-        print(".", end="")
+    # generate list of pairs from template
+    for i in range(0, len(template)-1):
+        pairs[template[i:i+2]] += 1
+
+    return pairs
+
+
+def get_elements(template):
+    elements = defaultdict(lambda: 0)
+    for e in template:
+        elements[e] += 1
+
+    return elements
+
+
+def iteration(insertions, pairs, elements):
+    out_pairs = defaultdict(lambda: 0)
+    out_elements = defaultdict(lambda: 0, elements)
+
+    for pair, count in pairs.items():
+        # insertion
+        insert = insertions[pair]
+        out_elements[insert] += count
+
+        # create new pairs based on insertion
+        p0 = f"{pair[0]}{insert}"
+        p1 = f"{insert}{pair[1]}"
+
+        # set new pairs count to count
+        out_pairs[p0] += count
+        out_pairs[p1] += count
+
+    return out_pairs, out_elements
 
 
 def main():
-    template, insertions = parse_data("./2021/day14/data/example_1")
+    template, insertions = parse_data("./2021/day14/data/full")
 
-    steps = 30
-    state = dllist(template)
+    steps = 40
 
     print(f"Template: {template}")
 
-    for step in range(1, steps+1):
-        iteration(step, insertions, state)
+    # get pairs and store them in the state
+    pairs = get_pairs(template)
+    elements = get_elements(template)
 
-    # count min and max values
-    counts = defaultdict(lambda: 0)
+    for _ in range(1, steps+1):
+        pairs, elements = iteration(insertions, pairs, elements)
 
-    for value in state.itervalues():
-        counts[value] += 1
+    sorted_elements = sorted(elements.items(), key=lambda t: t[1])
 
-    min_element = min(counts.items(), key=lambda t: t[1])
-    max_element = max(counts.items(), key=lambda t: t[1])
+    # print(sorted_elements)
+
+    min_element = sorted_elements[0]
+    max_element = sorted_elements[-1]
 
     print(f"min: {min_element}")
     print(f"max: {max_element}")
@@ -45,5 +70,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-    # TODO switch to counting the pairs rather than growing the string
