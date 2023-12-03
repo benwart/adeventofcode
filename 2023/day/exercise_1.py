1b05eebbef0bdf3be086713ba9647e6a0d1a9cc4
Ben Wart <benwart@gmail.com>
2023-12-03 08:56:58 -0800

    updated 2023 day template to include tests

--- 2023/day/exercise_1.py
+++ 2023/day/exercise_1.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python
+
+from pathlib import Path
+from typing import Iterable
+
+
+def parse_lines(filepath: Path) -> Iterable[str]:
+    with open(filepath, "r") as f:
+        for line in f:
+            yield line.strip()
+
+
+def main(filepath: Path):
+    print(filepath)
+
+
+if __name__ == "__main__":
+    main(Path(__file__).parent / "data" / "example_1")
