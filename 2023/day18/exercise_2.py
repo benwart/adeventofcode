543c8eaf18061abdad538133ddf6761b68b723d8
Ben Wart <benwart@gmail.com>
2023-12-18 14:21:40 -0800

    2023 day 18

---
+++ 2023/day18/exercise_2.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+
+from dataclasses import dataclass, field, InitVar
+from enum import StrEnum
+from pathlib import Path
+from re import compile
+from typing import Iterable
+
+from shapely import Point, LinearRing, Polygon
+
+INSTSTRUCTION_REGEX = compile(r"([UDLR]) (\d+) \(#(?P<steps>[a-f0-9]{5})(?P<direction>[0-3])\)")
+
+
+class Direction(StrEnum):
+    UP = "U"
+    DOWN = "D"
+    LEFT = "L"
+    RIGHT = "R"
+
+
+direction_map = {
+    "0": Direction.RIGHT,
+    "1": Direction.DOWN,
+    "2": Direction.LEFT,
+    "3": Direction.UP,
+}
+
+
+@dataclass
+class Instruction:
+    direction: Direction = field(init=False)
+    steps: int = field(init=False)
+    line: InitVar[str]
+
+    def __post_init__(self, line: str):
+        match = INSTSTRUCTION_REGEX.match(line)
+        if match is None:
+            raise ValueError(f"Invalid instruction: {line}")
+
+        self.direction = direction_map[match.group("direction")]
+        self.steps = int(f"0x{match.group("steps")}", 16)
+
+    def __repr__(self) -> str:
+        return f"Instruction[{str(self)}]"
+
+    def __str__(self) -> str:
+        return f"{self.direction} {self.steps} ({self.color})"
+
+
+def parse_lines(filepath: Path) -> Iterable[str]:
+    with open(filepath, "r") as f:
+        for line in f:
+            yield line.strip()
+
+
+def main(filepath: Path):
+    instructions = [Instruction(line) for line in parse_lines(filepath)]
+
+    # Create a polygon from the instructions
+    points: list[Point] = []
+    current_point = Point(0, 0)
+    for i in instructions:
+        if i.direction == Direction.UP:
+            new_point = Point(current_point.x, current_point.y + i.steps)
+        elif i.direction == Direction.DOWN:
+            new_point = Point(current_point.x, current_point.y - i.steps)
+        elif i.direction == Direction.LEFT:
+            new_point = Point(current_point.x - i.steps, current_point.y)
+        elif i.direction == Direction.RIGHT:
+            new_point = Point(current_point.x + i.steps, current_point.y)
+        else:
+            raise ValueError(f"Invalid direction: {i.direction}")
+
+        points.append(new_point)
+        current_point = new_point
+
+    edge = LinearRing(points)
+    pit = Polygon(edge).buffer(0.5, cap_style="square", join_style="mitre")
+    print(int(pit.area))
+
+
+if __name__ == "__main__":
+    main(Path(__file__).parent / "data" / "full")
