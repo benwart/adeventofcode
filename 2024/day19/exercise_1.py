86619306e9dc0b30fde1ee3a0a2d16396000e581
Ben Wart <benwart@gmail.com>
2024-12-19 16:52:54 -0800

    2024 day 19 using dynamic programming

--- 2024/day19/exercise_1.py
+++ 2024/day19/exercise_1.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
-from collections import deque
-from itertools import product
+from functools import cache
 from pathlib import Path
 from typing import Iterable
 
@@ -12,56 +11,27 @@ def parse_lines(filepath: Path) -> Iterable[str]:
             yield line.strip()
 
 
-def towels_per_stripe(pattern: str, i: int, towels: list[str]) -> list[str]:
-    return [towel for towel in towels if towel == pattern[i : i + len(towel)]]
+@cache
+def solve(pattern: str, towels: tuple[str, ...]) -> int:
+    if len(pattern) == 0:
+        return 1
+    else:
+        result: int = 0
+        for t in towels:
+            if pattern.startswith(t):
+                result += solve(pattern[len(t) :], towels)
 
-
-def solve(pattern: str, towels: list[str]) -> bool:
-    possible: list[list[str]] = []
-    for i, _ in enumerate(pattern):
-        matches: list[str] = towels_per_stripe(pattern, i, towels)
-
-        # if the first stripe doesn't have any matches then it's not possible
-        if i == 0 and len(matches) == 0:
-            # print(f"F: {pattern}")
-            return False
-
-        # make sure each stripe has at least one match (even if it's an empty string)
-        possible.append(matches if len(matches) else [" "])
-
-    for option in product(*possible):
-        output: deque = deque()
-        o: int = 0
-        while o < len(option):
-            opt: str = option[o]
-            if opt == " ":
-                break
-            output.append(opt)
-            o += len(opt)
-
-        combined: str = "".join(output)
-        if combined == pattern:
-            # print(f"T: {pattern} == {combined}")
-            return True
-
-    # print(f"F: {pattern} {possible}")
-    return False
+        return result
 
 
 def main(filepath: Path):
     line_iter: Iterable[str] = parse_lines(filepath)
 
-    towels: list[str] = [t.strip() for t in next(line_iter).strip().split(",")]
+    towels: set[str] = {t.strip() for t in next(line_iter).strip().split(",")}
     patterns: list[str] = [line.strip() for line in line_iter if len(line) > 0]
 
-    count: int = 0
-    for pattern in patterns:
-        result: bool = solve(pattern, towels)
-        print("+" if result else ".", end="")
-        if result:
-            count += 1
-
-    print(count)
+    solves: list[int] = [solve(p, tuple(towels)) for p in patterns]
+    print(len([s for s in solves if s > 0]))
 
 
 if __name__ == "__main__":
--- 2024/day19/exercise_2.py
+++ 2024/day19/exercise_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from dataclasses import dataclass, field
+from functools import cache
 from pathlib import Path
 from typing import Iterable
 
@@ -11,10 +11,28 @@ def parse_lines(filepath: Path) -> Iterable[str]:
             yield line.strip()
 
 
+@cache
+def solve(pattern: str, towels: tuple[str, ...]) -> int:
+    if len(pattern) == 0:
+        return 1
+    else:
+        result: int = 0
+        for t in towels:
+            if pattern.startswith(t):
+                result += solve(pattern[len(t) :], towels)
+
+        return result
+
+
 def main(filepath: Path):
-    for line in parse_lines(filepath):
-        print(line)
+    line_iter: Iterable[str] = parse_lines(filepath)
+
+    towels: set[str] = {t.strip() for t in next(line_iter).strip().split(",")}
+    patterns: list[str] = [line.strip() for line in line_iter if len(line) > 0]
+
+    solves: list[int] = [solve(p, tuple(towels)) for p in patterns]
+    print(sum([s for s in solves if s > 0]))
 
 
 if __name__ == "__main__":
-    main(Path(__file__).parent / "data" / "example_1")
+    main(Path(__file__).parent / "data" / "full")
